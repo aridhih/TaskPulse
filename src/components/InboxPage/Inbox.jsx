01360b312a4028a56fbfa4f64abcdf4ff9868e20
Hassan8176 <hfiaz216@gmail.com>
2024-12-30 11:08:24 +0500

    hassan work on inbox

--- src/components/InboxPage/Inbox.jsx
+++ src/components/InboxPage/Inbox.jsx
@@ -50,13 +50,13 @@ const Inbox = () => {
           {filterOpen && (
             <div className="absolute right-0 mt-6 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
               <div className="py-1">
-                <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex justify-between w-full">
+                <button className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex justify-between w-full">
                   Assigned to me <span className="text-gray-400">Shift + 1</span>
                 </button>
-                <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex justify-between w-full">
+                <button className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex justify-between w-full">
                   Mentioning me <span className="text-gray-400">Shift + 2</span>
                 </button>
-                <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex justify-between w-full">
+                <button className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex justify-between w-full">
                   Unread only <span className="text-gray-400">Shift + 3</span>
                 </button>
               </div>
